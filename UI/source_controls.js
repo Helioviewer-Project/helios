ab0991baac97a5880f6363b38e3eae59d233253b
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-08-17 13:56:05 -0400

    Add source manager

---
+++ UI/source_controls.js
@@ -0,0 +1,41 @@
+import DateRangePicker from './date_range_picker.js'';
+import DatasourcePicker from './datasource_picker.js';
+import ResolutionPicker from './resolution_picker.js';
+import Scene from '../Scene/scene.js';
+
+/**
+ * Manages current sources displayed in the scene
+ */
+class SourceManager {
+    constructor() {
+        /**
+         * Stores the IDs of layers that have been added to the scene
+         */
+        this._layers = [];
+    }
+
+    /**
+     * Adds a source to the scene
+     */
+    AddSource() {
+        let range = DateRangePicker.GetDateRange();
+        let source = DatasourcePicker.GetDatasource();
+        let resolution = ResolutionPicker.GetResolution();
+        let id = Scene.AddToScene(source, range.start, range.end, range.cadence, resolution);
+        this._layers.push(id);
+    }
+
+    /**
+     * Removes a source from the scene with the id returned from
+     * Scene.AddToScene
+     *
+     * @param {number} id ID of the source to remove
+     */
+    RemoveSource(id) {
+        Scene.RemoveFromScene(id);
+    }
+}
+
+let manager = new SourceManager();
+export default manager;
+
