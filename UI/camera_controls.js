bdf5ad7b0824e479d4a8dd6502d4355acecb502c
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-08-17 14:01:44 -0400

    Add camera controls class

---
+++ UI/camera_controls.js
@@ -0,0 +1,27 @@
+import Scene from '../Scene/scene.js';
+
+/**
+ * Camera controls provide UI functions for locking the
+ * camera to specific positions
+ */
+class CameraControls {
+    /**
+     * Locks the camera to a specific source's observer
+     *
+     * @param {number} id Scene model ID to lock camera on to
+     */
+    LockTo(id) {
+        Scene.LockCamera(id);
+    }
+
+    /**
+     * Unlocks the camera from its locked position
+     */
+    Unlock() {
+        Scene.UnlockCamera();
+    }
+}
+
+let controls = new CameraControls();
+export default controls;
+
