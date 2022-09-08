4593370782b59c5aa4d51e830c5197030309fec7
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-09-08 16:16:18 -0400

    Add build script & command

---
+++ build.sh
@@ -0,0 +1,17 @@
+# Exit if any command fails
+set -e
+dist_dir=dist
+
+# Clean any existing build
+rm -rf $dist_dir
+mkdir $dist_dir
+
+# Pack up javascript
+npx webpack
+
+# Copy dist files to dist folder
+cp helios_bundle.js $dist_dir
+cp index.html $dist_dir
+cp -r docs $dist_dir
+cp -r resources $dist_dir
+
