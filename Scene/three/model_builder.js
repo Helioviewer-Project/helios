eaa787492bb267c85bd8c39c64d9d7464f274311
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-09-09 14:49:02 -0400

    restore transparency threshold

--- Scene/three/model_builder.js
+++ Scene/three/model_builder.js
@@ -43,7 +43,7 @@ async function _GetBackside(texture, scale) {
             y_offset: {value: 0.0},
             backside: {value: true},
             opacity: {value: 1},
-            transparent_threshold: {value: 0.50}
+            transparent_threshold: {value: 0.15}
         },
         vertexShader: SolarVertexShader,
         fragmentShader: SolarFragmentShader
@@ -80,7 +80,7 @@ async function CreateHemisphereWithTexture(texture, jp2info) {
             y_offset: {value: 0.0},
             backside: {value: false},
             opacity: {value: 1},
-            transparent_threshold: {value: 0.55}
+            transparent_threshold: {value: 0.15}
         },
         vertexShader: SolarVertexShader,
         fragmentShader: SolarFragmentShader
