4caa5ec1ca9de250fa7888c9b3a42e9270904722
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-11-01 10:17:57 -0400

    Add event range lookup + some refactoring

---
+++ server/api/events.py
@@ -0,0 +1,38 @@
+from event_coord import get_event_coordinates, CoordinateSystem
+from hek import query_hek
+def lookup_hek_events(start_time: str, end_time: str):
+    """
+    Looks up HEK events that occur within the given time range
+
+    :param start_time: Beginning of time range to query
+    :type start_time: str
+    :param end_time: End of time range to query
+    :type end_time: str
+    :return: List of events
+    """
+    # Query all events
+    events = query_hek(start_time, end_time)
+    # List that is going to be returned
+    results = []
+    # For each event, transform them into usable coordinates
+    for event in events:
+        system = CoordinateSystem.from_str(event["event_coordsys"])
+        coordinates = get_event_coordinates(system, event["event_coord1"], event["event_coord2"], event["event_coord3"], event["event_starttime"].to_datetime(), event["obs_instrument"], event["event_coordunit"])
+        results.append({
+            "event": {
+                "coordinates": {
+                    "observer": {
+                        "x": coordinates["observer"][0],
+                        "y": coordinates["observer"][1],
+                        "z": coordinates["observer"][2],
+                    },
+                "event": coordinates["event"]
+                },
+                "data": {
+                    "id": event["kb_archivid"],
+                    "type": event["concept"]
+                }
+            }
+        })
+    return results
+
--- server/server.py
+++ server/server.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, make_response
-from get_heeq import convert_skycoords_to_heeq
-from event_coord import get_event_coordinates, CoordinateSystem
 from helios_exceptions import HeliosException
 from datetime import datetime
-from coordinate_lookup import get_observer_coordinate_by_id
 import json
 import logging
 logging.basicConfig(filename="helios_server.log", encoding="utf-8", level=logging.DEBUG)
@@ -21,63 +18,76 @@ def _parse_date(date_str: str):
     except ValueError as e:
         raise HeliosException(str(e))
 
+def _validate_input(parameter_list):
+    """
+    Validates that the HTTP request contains all the expected parameters
+    :param parameter_list: List of strings of names of the parameters needed
+    :type parameter_list: List[str]
+    :raises HeliosException: Error returned to user on failure
+    """
+    # Create a list of any parameters missing from the list
+    missing_list = []
+    for param in parameter_list:
+        if param not in request.args:
+            missing_list.append(param)
+    # If any are missing, create an error message
+    if (len(missing_list) > 0):
+        raise HeliosException("Missing parameters {}".format(missing_list.join(",")))
+
+def _exec(fn):
+    try:
+        return fn()
+    # The design here is any known error we should report to the user should be
+    # raised as a HeliosException. The error message is passed on to the user.
+    # Any other unexpected exception will be handled and the user will get a generic
+    # error message. Internally we will have logs that show what's going on.
+    except HeliosException as e:
+        logging.warning(e)
+        return _send_response({"error": str(e)})
+    except Exception as e:
+        logging.error(e)
+        return "An error occurred!: " + str(e)
+        return(str(e))
+        return _send_response({"error": "An internal error occurred, please file an issue with the timestamp at https://github.com/Helioviewer-Project/helios",
+            "timestamp": str(datetime.now())})
+
+
+
 # This takes the path to a local jp2 file and returns x, y, z (i.e. HEEQ) coordinates
 # relative to a know reference point. See get_heeq.py for details
 @app.route("/observer/position")
 def position_from_jp2():
-    try:
-        id = request.args["id"]
-        observer = get_observer_coordinate_by_id(id)
-        result = convert_skycoords_to_heeq(observer)
-        result_dict = {
-            "x": result[0],
-            "y": result[1],
-            "z": result[2]
-        }
-        return _send_response(result_dict)
-    # The design here is any known error we should report to the user should be
-    # raised as a HeliosException. The error message is passed on to the user.
-    # Any other unexpected exception will be handled and the user will get a generic
-    # error message. Internally we will have logs that show what's going on.
-    except HeliosException as e:
-        logging.warning(e)
-        return _send_response({"error": str(e)})
-    except Exception as e:
-        logging.error(e)
-        return _send_response({"error": "An internal error occurred, please file an issue with the timestamp at https://github.com/Helioviewer-Project/helios",
-            "timestamp": str(datetime.now())})
+    _validate_input(["id"])
+    from api.observer_position import get_observer_position
+    return _exec(lambda : get_observer_position(request.args["id"]))
 
+@app.route("/event")
+def get_events():
+    start = request.args["start"]
+    end = request.args["end"]
+
+    from api.events import lookup_hek_events
+    return _exec(lambda : lookup_hek_events(start, end))
 
 # This endpoint is used to convert between coordinate data from the HEK
 @app.route("/event/position")
 def event_position():
-    try:
-        coord_system = CoordinateSystem.from_str(request.args["system"])
-        coord1 = request.args["coord1"]
-        coord2 = request.args["coord2"]
-        coord3 = None
-        if ("coord3" in request.args):
-            coord3 = request.args["coord3"]
-        date = _parse_date(request.args["date"])
-        observatory = request.args["observatory"]
-        units = request.args["units"]
-        coordinates = get_event_coordinates(coord_system, coord1, coord2, coord3, date, observatory, units)
-        return _send_response({
-            "observer": {
-                "x": coordinates["observer"][0],
-                "y": coordinates["observer"][1],
-                "z": coordinates["observer"][2],
-            },
-            "event": coordinates["event"]
-        })
-    # The design here is any known error we should report to the user should be
-    # raised as a HeliosException. The error message is passed on to the user.
-    # Any other unexpected exception will be handled and the user will get a generic
-    # error message. Internally we will have logs that show what's going on.
-    except HeliosException as e:
-        logging.warning(e)
-        return _send_response({"error": str(e)})
-    except Exception as e:
-        logging.error(e)
-        return _send_response({"error": "An internal error occurred, please file an issue with the timestamp at https://github.com/Helioviewer-Project/helios",
-            "timestamp": str(datetime.now())})
+    _validate_input([
+        "system",
+        "coord1",
+        "coord2",
+        "date",
+        "observatory",
+        "units"
+    ])
+    coord_system = request.args["system"]
+    coord1 = request.args["coord1"]
+    coord2 = request.args["coord2"]
+    coord3 = None
+    if ("coord3" in request.args):
+        coord3 = request.args["coord3"]
+    date = _parse_date(request.args["date"])
+    observatory = request.args["observatory"]
+    units = request.args["units"]
+    from api.event_position import get_event_position
+    return _exec(lambda : get_event_position(coord_system, units, coord1, coord2, coord3, date, observatory))
