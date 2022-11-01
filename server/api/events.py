8e9aaa1f7417f08c5b6739a57a18273f7f0d1af5
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-11-01 13:47:51 -0400

    Update event data response format

--- server/api/events.py
+++ server/api/events.py
@@ -19,19 +19,17 @@ def lookup_hek_events(start_time: str, end_time: str):
         system = CoordinateSystem.from_str(event["event_coordsys"])
         coordinates = get_event_coordinates(system, event["event_coord1"], event["event_coord2"], event["event_coord3"], event["event_starttime"].to_datetime(), event["obs_instrument"], event["event_coordunit"])
         results["results"].append({
-            "event": {
-                "coordinates": {
-                    "observer": {
-                        "x": coordinates["observer"][0],
-                        "y": coordinates["observer"][1],
-                        "z": coordinates["observer"][2],
-                    },
-                "event": coordinates["event"]
+            "coordinates": {
+                "observer": {
+                    "x": coordinates["observer"][0],
+                    "y": coordinates["observer"][1],
+                    "z": coordinates["observer"][2],
                 },
-                "data": {
-                    "id": event["kb_archivid"],
-                    "type": event["concept"]
-                }
+                "event": coordinates["event"]
+            },
+            "info": {
+                "id": event["kb_archivid"],
+                "type": event["concept"]
             }
         })
     return results
