728647df9a2c595136d7697f48c3b556865c6859
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2022-11-10 14:29:01 -0500

    Skip tests querying the helios server

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 const config = {
     testPathIgnorePatterns: [
         "node_modules",
-        "geometry_service.test.js"
+        "geometry_service.test.js", // Geometry service is not used anymore
+        "position_finder.test.js"   // Position finder server runs on localhost, not available under test environment
     ]
 }
 
--- test/event_db.test.js
+++ test/event_db.test.js
@@ -6,7 +6,7 @@ test("Create range of dates", () => {
     // Expect 7 dates, October 1, 2, 3, 4, 5, 6, and 7.
     expect(dates.length).toBe(7);
 
-    let expected = new Date("2022-10-01 00:00:00Z");
+    let expected = new Date("2022-10-01 12:00:00Z");
     for (const date of dates) {
         expect(date.toISOString()).toBe(expected.toISOString());
         expected.setDate(expected.getDate() + 1);
@@ -15,12 +15,12 @@ test("Create range of dates", () => {
 
 test("Create range of dates given one day", () => {
     let dates = EventDB._DateRangeToIndividualDays(new Date("2022-10-01 01:55:00Z"), new Date("2022-10-01 23:00:00Z"));
-    let expected = new Date("2022-10-01 00:00:00Z");
+    let expected = new Date("2022-10-01 12:00:00Z");
     expect(dates[0].toISOString()).toBe(expected.toISOString());
 });
 
 
-test("Get events without duplicates", async () => {
+test.skip("Get events without duplicates", async () => {
     let events = await EventDB.GetEvents(new Date("2022-10-01 01:55:00Z"), new Date("2022-10-03 23:00:00Z"));
     let known_events = new Set();
     for (const e of events) {
