981d83a596d3c5a3092df1c7fa05f88ad161e67a
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-28 14:45:43 -0400

    Migrate app fn to app class

--- src/Helios.tsx
+++ src/Helios.tsx
@@ -1,10 +1,12 @@
 import Scene from './Scene/scene';
 import { createRoot } from 'react-dom/client';
-import React, { useState } from 'react';
+import React from 'react';
 import NavControls from './UI/navigation/controls';
 import { GetImageScaleForResolution } from './common/resolution_lookup.js';
 import config from './Configuration.js';
 import TimeDisplay from './UI/time_display.js';
+import { DataSource, DateRange } from './UI/navigation/control_tabs/data';
+import { ModelInfo } from './common/types';
 
 // /**
 //  * When the page first loads, users should see something besides black, so load the first available image
@@ -16,43 +18,72 @@ import TimeDisplay from './UI/time_display.js';
 
 // LoadDefaultImage();
 
-
-
 // Defined in main HTML, not React
 const scene = new Scene('js-helios-viewport');
 
-/**
- * Add
- */
-let addSourceLayer = async (source, dateRange) => {
-    if (dateRange.start > dateRange.end) {
-        alert('Start time must be before end time');
-    } else {
-        let image_scale = GetImageScaleForResolution(config.default_texture_resolution, source.value);
-        let layer = await scene.AddToScene(source.value, dateRange.start, dateRange.end, dateRange.cadence, image_scale, 1);
-        console.log("TODO: Need to add the new layer to the UI")
-    }
+type AppState = {
+    sceneTime: Date,
+    layers: ModelInfo[]
 }
 
-function App() {
-    const [sceneTime, setSceneTime] = useState(null);
-    // Make sure this is only ever called once.
-    if (sceneTime == null) {
+/**
+ * Entry point for the Helios app.
+ * Intializes the scene, builds controls, and registers event handlers.
+ */
+class App extends React.Component<{}, AppState> {
+    constructor(props) {
+        super(props);
+        // Register the listener to update the React state any time the scene time changes.
+        // This happens either from data loading or animation
+        let firstRun = true
         scene.RegisterTimeUpdateListener((newTime) => {
-            setSceneTime(newTime);
+            console.log("Setting time to", newTime)
+            if (firstRun) {
+                this.state = {
+                    sceneTime: newTime,
+                    layers: []
+                }
+                firstRun = false
+            } else {
+                this.setState({sceneTime: newTime})
+            }
         })
+
+        this.AddLayer = this.AddLayer.bind(this);
     }
-    return (
-        <div>
-            <TimeDisplay time={sceneTime} onTimeChange={time => scene.SetTime(time)} />
-            <NavControls
-                onAddData={addSourceLayer}
-                GetSceneTime={() => scene.GetTime()}
-                GetSceneTimeRange={() => scene.GetTimeRange()}
-                GetMaxFrameCount={() => scene.GetMaxFrameCount()}
-                SetSceneTime={(date) => scene.SetTime(date)}/>
-        </div>
-    )
+
+    /**
+     * Adds a new model layer to the current scene
+     * @param source Source for the new layer
+     * @param dateRange Range to import data over
+     */
+    async AddLayer(source: DataSource, dateRange: DateRange) {
+        if (dateRange.start > dateRange.end) {
+            alert('Start time must be before end time');
+        } else {
+            let image_scale = GetImageScaleForResolution(config.default_texture_resolution, source.value);
+            let layer = await scene.AddToScene(source.value, dateRange.start, dateRange.end, dateRange.cadence, image_scale, 1);
+            this.setState({
+                layers: this.state.layers.concat(layer)
+            })
+        }
+    }
+
+    render(): React.ReactNode {
+        console.log(this.state.layers)
+        return (
+            <div>
+                <TimeDisplay time={this.state.sceneTime} onTimeChange={time => scene.SetTime(time)} />
+                <NavControls
+                    onAddData={this.AddLayer}
+                    Layers={this.state.layers}
+                    GetSceneTime={() => scene.GetTime()}
+                    GetSceneTimeRange={() => scene.GetTimeRange()}
+                    GetMaxFrameCount={() => scene.GetMaxFrameCount()}
+                    SetSceneTime={(date) => scene.SetTime(date)}/>
+            </div>
+        )
+    }
 }
 
 let root = createRoot(document.getElementById('app'));
