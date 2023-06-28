d5a4d691b511eeeafb853f621c3be1bbce3ab965
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-28 11:13:34 -0400

    Update build script to add css
    Copies css from packages installed with npm to the public folder.

--- build.sh
+++ build.sh
@@ -11,9 +11,12 @@ npx webpack
 # make docs
 npm run docs
 
+# Copy libraries from node modules to public folder
+resources_dir=resources/lib
+cp node_modules/flatpickr/dist/flatpickr.min.css $resources_dir
+
 # Copy dist files to dist folder
 cp helios_bundle.js $dist_dir
 cp index.html $dist_dir
 cp -r docs $dist_dir
 cp -r resources $dist_dir
-
