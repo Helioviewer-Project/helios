e453ea4f702a1ec805b0f35609b083fe2450be89
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-29 11:44:07 -0400

    update datepicker styles

--- src/UI/navigation/control_tabs/components/date_range_picker.js
+++ src/UI/navigation/control_tabs/components/date_range_picker.js
@@ -1,43 +1,40 @@
 import {ToLocalDate, ToUTCDate} from "../../../../common/dates";
 import Flatpickr from "react-flatpickr";
 import React, { useEffect, useState } from 'react';
-
-/**
- * Configuration for flatpickr datepickers.
- * See https://flatpickr.js.org/options/
- */
-const DatePickerConfig = {
-    enableTime: true,
-    enableSeconds: true,
-    mode: "single",
-    time_24hr: true
-}
+import Input from "../../../components/input/input";
+import input_css from "../../../components/input/input.css";
 
 function frames2cadence(start, end, frames) {
     return (((end - start) / frames) / 1000);
 }
 
-
 export default function DateRangePicker({value, setValue}) {
     let currentRange = value;
-    let defaultValue = 60;
+    let [frameCount, setFrameCount] = useState(60)
     useEffect(() => {
-        currentRange.cadence = frames2cadence(currentRange.start, currentRange.end, defaultValue);
+        updateCadence(frameCount);
     }, [])
-    function updateCadence(e) {
-        currentRange.cadence = frames2cadence(currentRange.start, currentRange.end, e.target.value);
-        setValue(currentRange)
+    function updateCadence(newFrameCount) {
+        newFrameCount = parseFloat(newFrameCount);
+        if (!isNaN(newFrameCount)) {
+            currentRange.cadence = frames2cadence(currentRange.start, currentRange.end, newFrameCount);
+            setValue(currentRange);
+            setFrameCount(newFrameCount);
+        } else {
+            setFrameCount("");
+        }
     }
     return (
-        [
-            <label key={0} htmlFor="js-start-date-picker">Start Date &amp; Time</label>,
-            <Flatpickr key={1} data-enable-time value={ToLocalDate(currentRange.start)} onChange={([date]) => {currentRange.start = ToUTCDate(date); setValue(currentRange)}} />,
-
-            <label key={2} htmlFor="js-end-date-picker">End Date &amp; Time</label>,
-            <Flatpickr key={3} data-enable-time value={ToLocalDate(currentRange.end)} onChange={([date]) => {currentRange.end = ToUTCDate(date); setValue(currentRange)}} />,
-
-            <label key={4} htmlFor="js-date-range-frames" name="Number of images to download in this time range">Number of Frames</label>,
-            <input key={5} id="js-date-range-frames" defaultValue={defaultValue} type="number" onChange={updateCadence} />
-        ]
+        <>
+            <div className={input_css.container}>
+                <Flatpickr className={input_css.input} data-enable-time value={ToLocalDate(currentRange.start)} onChange={([date]) => {currentRange.start = ToUTCDate(date); setValue(currentRange)}} />
+                <label className={input_css.label}>Start Date &amp; Time</label>
+            </div>
+            <div className={input_css.container}>
+                <Flatpickr className={input_css.input} data-enable-time value={ToLocalDate(currentRange.end)} onChange={([date]) => {currentRange.end = ToUTCDate(date); setValue(currentRange)}} />
+                <label className={input_css.label}>End Date &amp; Time</label>
+            </div>
+            <Input label="Number of Frames" type="number" value={frameCount} onChange={updateCadence} />
+        </>
     )
 }
