7c064022689d627108d0bb5ace52fbdfab8fddea
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-06-29 12:04:18 -0400

    update style on animation tab

--- src/UI/components/input/input.tsx
+++ src/UI/components/input/input.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import css from './input.css'
 
-type InputProps = {
-    label: string,
-    type: string,
-    value: any,
-    onChange: (value: any) => void
-}
+interface InputProps {
+    label: string;
+    type: string;
+    value: any;
+    style?: Object | null;
+    onChange: (value: any) => void;
+};
 
 export default function Input(props: InputProps): React.JSX.Element {
-    return <div className={`${css.container}`}>
+    return <div style={props.style} className={`${css.container}`}>
         <input id={window.atob(props.label)} className={css.input} type={props.type} value={props.value} onChange={(e) => props.onChange(e.target.value)} />
         <label className={css.label} htmlFor={window.atob(props.label)}>{props.label}</label>
     </div>
