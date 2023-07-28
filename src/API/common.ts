5ee0b63fe1827055e73eb57e652f49c32c0b907a
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-07-28 12:09:46 -0400

    Update coordinate mapping between API and web

--- src/API/common.ts
+++ src/API/common.ts
@@ -2,13 +2,18 @@ import Coordinates from "../common/coordinates";
 
 /**
  * Converts an object with x, y, z values to our Coordinate object
+ * Important mapping of API/Python coordinate system to Web/Threejs system.
+ * X : Y
+ * Y : Z
+ * Z : X
+ * Threejs : Python
  * @returns Coordinates
  */
 function ToCoordinates(response: Object): Coordinates {
-    let x = response["x"];
+    let x = response["y"];
     let y = response["z"];
-    let z = response["y"];
-    return new Coordinates(-x, y, z);
+    let z = response["x"];
+    return new Coordinates(x, y, z);
 }
 
 export { ToCoordinates };
