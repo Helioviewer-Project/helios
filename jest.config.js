b20d716b57b6e0dbe72a926e958b46385aedf6fb
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-10-13 13:48:59 -0400

    Enable testing react components

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ const config = {
         "node_modules",
         "geometry_service.test.js", // Geometry service is not used anymore
         "position_finder.test.js"   // Position finder server runs on localhost, not available under test environment
-    ]
+    ],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+          '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': 'identity-obj-proxy',
+      },
 }
 
 module.exports = config;
