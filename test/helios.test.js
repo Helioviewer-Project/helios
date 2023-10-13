3aedbdaaea887a64b42268837bdacc9965065658
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-10-12 21:22:17 -0400

    Add test to prettier check

--- test/helios.test.js
+++ test/helios.test.js
@@ -1,40 +1,41 @@
-import Configuration from '../src/Configuration.js';
-import { Helios } from '../src/API/helios';
-Configuration.helios_api_url = 'http://127.0.0.1:5000/';
+import Configuration from "../src/Configuration.js";
+import { Helios } from "../src/API/helios";
+Configuration.helios_api_url = "http://127.0.0.1:5000/";
 
-test('Saving a scene', async () => {
-    let testLayers = [{
-        source: 13,
-        start: new Date(),
-        end: new Date(),
-        cadence: 86400,
-        scale: 0.6
-    }];
+test("Saving a scene", async () => {
+    let testLayers = [
+        {
+            source: 13,
+            start: new Date(),
+            end: new Date(),
+            cadence: 86400,
+            scale: 0.6,
+        },
+    ];
     global.window = {
         // This doesn't do a deep copy like structured clone, but it is good enough for this test.
-        structuredClone: (obj) => obj
-    }
+        structuredClone: (obj) => obj,
+    };
     let result = await Helios.SaveScene({
         created_at: new Date(),
         start: new Date(),
         end: new Date(),
         shared: false,
         thumbnail: "Test thumbnail",
-        layers: testLayers
+        layers: testLayers,
     });
-    expect(result).not.toHaveProperty('error');
-    expect(typeof result).toBe('number');
-})
+    expect(result).not.toHaveProperty("error");
+    expect(typeof result).toBe("number");
+});
 
-test('Loading a scene', async () => {
+test("Loading a scene", async () => {
     let result = await Helios.LoadScene(1);
-    expect(result).not.toHaveProperty('error');
-    expect(result).toHaveProperty('id');
-    expect(result).toHaveProperty('layers');
-    expect(result['layers'].length).toBeGreaterThan(0);
-})
-test('Test the date', async () => {
-    let result = await Helios.get_field_lines_gong([new Date()])
+    expect(result).not.toHaveProperty("error");
+    expect(result).toHaveProperty("id");
+    expect(result).toHaveProperty("layers");
+    expect(result["layers"].length).toBeGreaterThan(0);
+});
+test("Test the date", async () => {
+    let result = await Helios.get_field_lines_gong([new Date()]);
     expect(Array.isArray(result)).toBe(true);
-
-})
+});
--- test/position_finder.test.js
+++ test/position_finder.test.js
@@ -1,6 +1,8 @@
-import PositionFinder from '../Images/position_finder.js';
+import PositionFinder from "../Images/position_finder.js";
 
-test('GetPosition', async () => {
+test("GetPosition", async () => {
     let scene_position = await PositionFinder.GetPosition(86417705);
-    expect(JSON.stringify(scene_position)).toBe(`{"x":117.10125976818252,\"y\":-7.880125864472047,\"z\":88.701916413244}`);
+    expect(JSON.stringify(scene_position)).toBe(
+        `{"x":117.10125976818252,\"y\":-7.880125864472047,\"z\":88.701916413244}`
+    );
 });
