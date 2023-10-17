41bd74b59f16fafd5877e0a88aa50d1b583f2980
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-10-17 15:56:24 -0400

    Add model interface & documentation

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,6 @@
+import { Object3D } from "three";
 import Model from "../Scene/model";
+import Coordinates from "./coordinates";
 
 // See scene.js JSDoc reference for this type.
 type ModelInfo = {
@@ -32,4 +34,61 @@ interface DateRange {
     cadence: number;
 }
 
-export { ModelInfo, SceneLayer, DateRange };
+/**
+ * A model represents generic 3D model information that can be rendered.
+ * The model must define certain functions in order to be supported by the Scene.
+ *
+ */
+interface Model {
+    /**
+     * Optionally get the threejs model to render in the scene.
+     * If an object is returned, it will be added to the scene.
+     * For more advanced models, this may return null, and the rendering can be done within the model code.
+     * @returns Model object or null
+     */
+    GetModel: () => Object3D | null;
+
+    /**
+     * Executed when the scene time is updated. The model should update itself to represent the given time.
+     * Typically this is done during animation, so it should be fast, don't perform any long running tasks here (i.e. API requests).
+     * @param date New scene time.
+     */
+    SetTime: (date: Date) => void;
+
+    /**
+     * Sets the model's layer order. If sharing space with other models,
+     * the layer order determines which should be on top.
+     * Higher numbers should be rendered on top of lower numbers.
+     * @param number Layer order / "Z index"
+     */
+    SetLayerOrder: (number) => void;
+
+    /**
+     * This should return the number of "frames" / "time steps" associated with this data.
+     * This is used by the animation engine to determine how many frames should displayed.
+     * @returns
+     */
+    GetFrameCount: () => number;
+
+    /**
+     * Get the observer coordinate used for orienting this model.
+     * The model will face this coordinate when rendered.
+     * @returns x, y, z coordinate object
+     */
+    GetObserverPosition: () => Coordinates;
+
+    /**
+     * The model should update its opacity based on the given value (0 to 1)
+     * @param opacity The new opaqueness
+     */
+    SetOpacity: (opacity: number) => void;
+
+    /**
+     * Executed when the model is removed from the scene.
+     * This should cleanup any used threejs resources.
+     * If the rendering is self-managed, this should also remove the model from the scene.
+     */
+    dispose: () => void;
+}
+
+export { Model, ModelInfo, SceneLayer, DateRange };
