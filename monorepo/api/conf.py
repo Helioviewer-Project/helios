861d71114ffa3540a6b05304c5c15bb0ae8019f1
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-11-27 12:37:34 -0500

    Debug prints for ci environment

--- monorepo/api/conf.py
+++ monorepo/api/conf.py
@@ -15,10 +15,13 @@ from configparser import ConfigParser
 # Then default to config.example.ini
 _parser = ConfigParser()
 if "CONFIG_FILE_PATH" in os.environ:
+    print("Reading config from environment")
     _parser.read(os.environ["CONFIG_FILE_PATH"])
 elif os.path.exists("config.ini"):
+    print("Reading config from config.ini")
     _parser.read("config.ini")
 else:
+    print("Reading config from config.example.ini")
     _parser.read("config.example.ini")
 
 
