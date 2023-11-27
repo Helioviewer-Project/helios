b015157f59635ad4a4c6a527d250771f5e7798fa
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-11-27 12:32:09 -0500

    Restore API url

--- monorepo/app/src/Configuration.js
+++ monorepo/app/src/Configuration.js
@@ -35,7 +35,7 @@ class Config {
         /**
          * Helios API server url
          */
-        this.helios_api_url = "http://localhost:5000/";
+        this.helios_api_url = "https://api.gl.helioviewer.org/";
 
         /**
          * HTML ID of the element to use for the 3js viewport
